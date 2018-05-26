b115cfdc780d812be7690a471d1bd3bad3feb29e
Hiroshi Sakurai <an.olive.tree@gmail.com>
2018-05-26 22:15:34 +0900

    specify output name in command line argument

--- kindlepdf.sh
+++ kindlepdf.sh
@@ -1,26 +1,54 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+	echo Specify output name
+	echo
+	echo Usage: $0 '<outname>'
+	exit 0
+fi
+
+outname=$1
+
+rm -rf $outname
+mkdir -p $outname
+cd $outname
+
 SWIPE_MS=30 # should not be too fast
-FAILMAX=3
+FAILMAX=3 # detect the end of a book when screenshot doesn't change for this amount of times
 
 capture_png () {
 	fname=$1
+	#echo get catpure
+
 	# adb shell uses pty which mangles binary output.
 	# https://stackoverflow.com/questions/13578416/read-binary-stdout-data-from-adb-shell
-	echo get catpure
 	adb exec-out screencap -p > $fname.tmp
+
 	# remove unnecessary text like "capture from screenshot!"
 	ofs=`LANG=C grep -obUaP "\x89PNG" $fname.tmp | cut -f 1 -d ':'`
 	ofs=$((ofs + 1))
-	echo ofs=$ofs
+	#echo ofs=$ofs
 	tail -c +$ofs $fname.tmp > $fname
 	rm $fname.tmp
+
+	echo $fname
+}
+
+calc_filehash() {
+	fname=$1
+	cropped=$1.cropped.bmp # converting to png doesn't create identical file, so convert to bmp
+
+	# remove status area so that it ignores date or battery change
+	# the height of tall cutout is around 126 on Pixel. I guess 126 is enough.
+	convert $fname -crop +0+126 $cropped
+	md5sum $cropped | awk '{print $1}'
+	rm $cropped
 }
 
 rm -f *.png
 
 capture_png 00000.png
-hash0=`md5sum 00000.png|awk '{print $1}'`
+hash0=$(calc_filehash 00000.png)
 
 w=`identify -ping -format "%w" 00000.png`
 h=`identify -ping -format "%h" 00000.png`
@@ -32,10 +60,11 @@ swipeY=$((h / 2))
 adb shell input swipe $swipeL $swipeY $swipeR $swipeY $SWIPE_MS
 sleep 2
 capture_png 00001.png
-hash1=`md5sum 00001.png|awk '{print $1}'`
+hash1=$(calc_filehash 00001.png)
 
 # if image didn't change, swipe the opposite direction
 if [ "$hash0" = "$hash1" ];then
+	R2L=0
 	swipeS=$swipeR
 	swipeE=$swipeL
 
@@ -43,6 +72,7 @@ if [ "$hash0" = "$hash1" ];then
 	sleep 2
 	capture_png 00001.png
 else
+	R2L=1
 	swipeS=$swipeL
 	swipeE=$swipeR
 fi
@@ -59,19 +89,20 @@ for i in $(seq -f "%05g" 2 99999)
 do
 	while true; do
 		fname=$i.png
-		echo $fname
 		adb shell input swipe $swipeS $swipeY $swipeE $swipeY $SWIPE_MS
 		sleep 2
 		capture_png $fname
-		hash=`md5sum $fname|awk '{print $1}'`
+		hash=$(calc_filehash $fname)
 		if [ "$prevhash" = "$hash" ];then
 			failcount=$((failcount+1))
 			if [ $failcount -gt $FAILMAX ]; then
+				# skip the last page as it's likely an ad 
+				echo Delete $prevfname and $fname
 				rm $prevfname
 				rm $fname
 				break
 			fi
-			echo failcount=$failcount
+			echo Identical png. It\'s an end of a book? Retrycnt=$failcount
 			continue
 		fi
 		failcount=0
@@ -88,6 +119,11 @@ done
 # Don't know how many gray shades e-ink supports. 256 should be enough.
 mogrify -format png -colorspace Gray -colors 256 *.png
 
-convert `ls *.png | sort -n` file.pdf
+if [ $R2L = 0 ]; then
+	convert `ls *.png | sort -n` $outname.pdf
+else
+	convert 00000.png `ls *.png | sort -r -n` $outname.pdf
+fi
 
-
+# exit outout directory
+cd ../
